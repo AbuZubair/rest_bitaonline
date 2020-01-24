1d6c6835a061d00c089bead1de2bb8a773a96101
Aldo Hambali <aldo_kun@yahoo.com>
2020-01-24 09:59:24 +0700

    user approval api

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -24,8 +24,108 @@ class User extends REST_Controller {
 
         $this->response(array('status' => 200, 'message' => 'Sukses', 'data' => $data),200);
     		
-	}
+    }
+    
+    
 	
+	public function get_all_data_pending_get()
+    {
+		// var_dump($_POST); die();
+        $data = $this->User_model->get_all_data_pending($this->get('q'));
 
+        // $i=0;
+        // foreach ($data as $value) {
+        //     $kota_ = substr(strstr($value->name," "), 1);
+        //     $kota = strtolower($kota_);
+        //     $data[$i]->name = ucwords($kota);
+        //     $i++;
+        // }
+
+        $this->response(array('status' => 200, 'message' => 'Sukses', 'data' => $data),200);
+    		
+    }
+
+     
+    public function process_approve_user_post(){
+        log_message('debug','process_approve_user_post data : '.json_encode($this->post()));
+        $id = $this->post('id');
+        $user_id = $this->post('user_id');
+        if($id =='' || $user_id = ''){
+            $resp = array('message' => 'Data tidak valid, Proses Gagal Dilakukan');
+            $this->response($resp);
+        }     
+        else {
+            try {
+      
+                $this->db->trans_begin();
+    
+                $update = $this->User_model->update('user', array('is_approved' =>'Y'), array('user_id' => $id));
+
+        
+                log_message('debug','process_approve_user_post submitdata : '.json_encode($update));
+
+                if ($this->db->trans_status() === FALSE)
+                {
+                    $this->db->trans_rollback();
+                    log_message('debug','process_approve_user_post submitdata trans_rollback');
+                    $this->response(array('status' => 301, 'message' => 'Maaf Proses Gagal Dilakukan'),301);
+                }
+                else
+                {
+                    $this->db->trans_commit();
+                    log_message('debug','process_approve_user_post submitdata trans_commit');
+                    $this->response(array('status' => 200, 'message' => 'Proses Berhasil Dilakukan', 'data' => $id),200);
+                }
+    
+    
+            } catch (Exception $e) {
+                log_message('debug','process_approve_user_post submitdata try error 500'); 
+               $this->response( $e->getMessage() ,500);
+            }
+        }
+
+    }
+
+
+    public function process_reject_user_post(){
+        log_message('debug','process_reject_user_post data : '.json_encode($this->post()));
+        $id = $this->post('id');
+        $user_id = $this->post('user_id');
+
+        if($id =='' || $user_id = ''){
+            $resp = array('message' => 'Data tidak valid, Proses Gagal Dilakukan');
+            $this->response($resp);
+        }     
+        else {
+            try {
+      
+                $this->db->trans_begin();
+    
+                $update = $this->User_model->update('user', array('is_deleted' =>'Y'), array('user_id' => $id));
+
+        
+                log_message('debug','process_reject_user_post submitdata : '.json_encode($update));
+
+                if ($this->db->trans_status() === FALSE)
+                {
+                    $this->db->trans_rollback();
+                    log_message('debug','process_reject_user_post submitdata trans_rollback');
+                    $this->response(array('status' => 301, 'message' => 'Maaf Proses Gagal Dilakukan'),301);
+                }
+                else
+                {
+                    $this->db->trans_commit();
+                    log_message('debug','process_reject_user_post submitdata trans_commit');
+                    $this->response(array('status' => 200, 'message' => 'Proses Berhasil Dilakukan', 'data' => $id),200);
+                }
+    
+    
+            } catch (Exception $e) {
+                log_message('debug','process_reject_user_post submitdata try error 500'); 
+               $this->response( $e->getMessage() ,500);
+            }
+        }
+
+    }
 }
 ?>
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -16,7 +16,7 @@ class User_model extends CI_Model {
 		return $this->db->insert_id();;
 	}
 
-    public function update($table,$where, $data)
+    public function update($table,$data, $where)
 	{
 		$this->db->update($table, $data, $where);
 		return $this->db->affected_rows();
@@ -31,6 +31,21 @@ class User_model extends CI_Model {
                     WHERE user.is_active = 'Y' AND user.is_deleted = 'N' LIMIT ".$n."";
         return $this->db->query($query)->result();
     }
-    
+
+    public function get_all_data_pending()
+    {
+        // $query = "SELECT user_profile.*,user.*,regencies.name
+        //             FROM user_profile
+        //             left join user on user.user_id=user_profile.user_id
+        //             LEFT JOIN regencies on user_profile.regency = regencies.id
+        //             WHERE user.is_approved = 'N'";
+        $query = "SELECT *
+        FROM user 
+        WHERE is_approved = 'N' and is_deleted='N' order by created_date desc";
+        $result = $this->db->query($query)->result();    
+        log_message('debug','get_all_data_pending query : '.json_encode($this->db->last_query()));
+        return $result;
+        
+    }
 
 }
